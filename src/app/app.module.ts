0990f66167b9b183837cff3bb5757f47b375ac6c
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2021-10-27 19:09:18 +0300

    imported proper modules, Added Routes, Installed Routes, used Router-Outlet and Router-Link, Added New Components, Added New Routing Strategies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { ProtectedComponent } from './protected/protected.component';
+import { ProductsComponent } from './products/products.component';
+
+const routes: Routes = [
+  // basic routes
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'contactus', redirectTo: 'contact' },
+
+  // authentication demo
+  // { path: 'login', component: LoginComponent },
+  // { path: 'protected', component: ProtectedComponent, canActivate: [LoggedInGuard] },
+
+  // // nested 
+  // { path: 'products', component: ProductsComponent, children: childRoutes }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    LoginComponent,
+    ProtectedComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    //ProductsModule
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    //AUTH_PROVIDERS,
+    //LoggedInGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
